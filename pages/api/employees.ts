7ab3b482f84774c800f36eaa491a656a2a73d9d5
mikecrv2019-bit <mikecrv2019@gmail.com>
2026-05-03 12:06:34 -0400

    Add new employee records to API
    Added new employee records to the employees list.

--- pages/api/employees.ts
+++ pages/api/employees.ts
@@ -6,6 +6,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     { id: "E002", nombre: "María López", area: "Almacén", turno: "Tarde" },
     { id: "E003", nombre: "Carlos Ruiz", area: "Empaque", turno: "Noche" },
     { id: "E004", nombre: "Jaime Lopez", area: "Inlet", turno: "Mañana" },
+    { id: "E005", nombre: "Gabriel Varela", area: "Inlet", turno: "Mañana" }
+{ id: "E006", nombre: "Freddy Rivera", area: "TAR", turno: "Mañana" }
+{ id: "E007", nombre: "Alexander James", area: "Inlet", turno: "Mañana" }
+{ id: "E008", nombre: "Maria Pinzon", area: "Inlet", turno: "Mañana" }
+ { id: "E009", nombre: "Diana Donato", area: "Inlet", turno: "Mañana" }   
   ]
   res.status(200).json(employees)
 }
